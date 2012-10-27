2d442f59cfd1862aa82a685d575dc5dd674f336b
Jeremy Kao <j7kao@ucsd.edu>
2012-10-26 21:53:00 -0700

    Adding all

---
+++ src/com/kaiser/kpaftervisit/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.kaiser.kpaftervisit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        
+        Handler handler = new Handler();
+        
+        // run a thread after 2 seconds to start the home screen
+        handler.postDelayed(new Runnable() {
+ 
+            public void run() {
+ 
+                // make sure we close the splash screen so the user won't come back when it presses back key
+ 
+                finish();
+                // start the home screen
+ 
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(intent);
+ 
+            }
+ 
+        }, 2000); // time in milliseconds (1 second = 1000 milliseconds) until
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.splash_screen, menu);
+        return true;
+    }
+}
